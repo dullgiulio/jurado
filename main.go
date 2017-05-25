85a11b1555d5dee67e1ff1f40b8783faf2ba4b1e
Giulio Iotti <dullgiulio@gmail.com>
2017-05-25 11:52:05 +0000

    finish main structure for IPC

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,8 +13,31 @@ import (
 	"time"
 )
 
+type TestResult struct {
+	From    string // from which host
+	Host    string // observed host
+	Product string
+	Group   string
+	Date    time.Time
+	Results []CheckResult
+	Error   error
+	check   *Check
+}
+
+func newTestResult(from, product string, date time.Time, res []CheckResult, c *Check) *TestResult {
+	return &TestResult{
+		From:    from,
+		Product: product,
+		Host:    c.Host,
+		Group:   c.Group,
+		Date:    date,
+		Results: res,
+		check:   c,
+	}
+}
+
 type CheckResult struct {
-	Check  *Check
+	check  *Check
 	Status int // Use HTTP statuses
 	Time   time.Time
 	Error  string
@@ -20,7 +45,7 @@ type CheckResult struct {
 
 func NewCheckResult(ch *Check, status int) *CheckResult {
 	return &CheckResult{
-		Check:  ch,
+		check:  ch,
 		Time:   time.Now(),
 		Status: status,
 	}
@@ -31,7 +56,7 @@ type testFn func(ch *Check, args TestArgs) (*CheckResult, error)
 type testMap map[string]testFn
 
 type tester interface {
-	test(*Check) ([]*CheckResult, error)
+	test(*Check) ([]CheckResult, error)
 }
 
 func newTester(srv string, ch *Check) (tester, error) {
@@ -44,17 +69,16 @@ func newTester(srv string, ch *Check) (tester, error) {
 }
 
 type httpTester struct {
-	host string
+	host   string
 	client *http.Client
 }
 
-// TODO implement tester interface
-func (h *httpTester) test(ch *Check) ([]*CheckResult, error) {
+func (h *httpTester) test(ch *Check) ([]CheckResult, error) {
 	tm := testMap(make(map[string]testFn))
 	tm["http-check-status"] = h.testHttpCheckStatus
 	tm["http-body-contains"] = h.testHttpBodyContains
 
-	res := make([]*CheckResult, len(ch.Tests))
+	res := make([]CheckResult, len(ch.Tests))
 	i := 0
 	for tname := range ch.Tests {
 		fn, ok := tm[tname]
@@ -66,7 +90,7 @@ func (h *httpTester) test(ch *Check) ([]*CheckResult, error) {
 			// TODO: turn errors into results
 			log.Printf("Error: in test %s: %s\n", tname, err)
 		}
-		res[i] = r
+		res[i] = *r
 		i++
 	}
 	return res, nil
@@ -89,13 +113,35 @@ type TestInfo map[string]TestArgs
 
 type hostname string
 
-type config map[hostname]struct{ Options ConfOptions; Products ConfProducts }
+type config map[hostname]struct {
+	Options  ConfOptions
+	Products ConfProducts
+}
 
 type ConfProducts map[string][]Check
 
+func (p ConfProducts) runCheckers(host string, in chan<- *TestResult) {
+	for prodName, prods := range p {
+		for i := range prods {
+			check := &prods[i]
+			tr, err := check.run(prodName, host)
+			if err != nil {
+				tr.Error = err
+			}
+			in <- tr
+		}
+	}
+}
+
 type ConfOptions struct {
-	File string
+	File    string
 	Remotes []string
+}
+
+func (o *ConfOptions) addRemotesPath(path string) {
+	for i := range o.Remotes {
+		o.Remotes[i] = o.Remotes[i] + path
+	}
 }
 
 type Check struct {
@@ -107,25 +153,16 @@ type Check struct {
 	// TODO: Checked from etc
 }
 
-func (c *Check) run() ([]*CheckResult, error) {
+func (c *Check) run(product, host string) (*TestResult, error) {
 	tester, err := newTester(c.Service, c)
 	if err != nil {
 		return nil, err
 	}
-	return tester.test(c)
-}
-
-func postCheckResult(rw http.ResponseWriter, req *http.Request) {
-	req.ParseForm()
-	log.Println(req.Form)
-	cr := &CheckResult{}
-	for key, _ := range req.Form {
-		err := json.Unmarshal([]byte(key), &cr)
-		if err != nil {
-			log.Println(err.Error())
-		}
+	res, err := tester.test(c)
+	if err != nil {
+		return nil, err
 	}
-	fmt.Printf("%+v\n", cr)
+	return newTestResult(host, product, time.Now(), res, c), nil
 }
 
 func readHttpFile(url string) ([]byte, error) {
@@ -183,6 +220,152 @@ func (c config) forHostname(h hostname) (ConfProducts, *ConfOptions, error) {
 	return cf.Products, &cf.Options, nil
 }
 
+type status map[string]ProdStatus
+
+func makeStatus() status {
+	return make(map[string]ProdStatus)
+}
+
+// Append and keep latest N for each test (TODO)
+func (s status) appendTestResult(host, product, group string, tr *TestResult) {
+	if _, ok := s[host]; !ok {
+		s[host] = makeProdStatus()
+	}
+	if _, ok := s[host][product]; !ok {
+		s[host][product] = makeGroupStatus()
+	}
+	if _, ok := s[host][product][group]; !ok {
+		s[host][product][group] = make([]*TestResult, 1)
+	}
+	s[host][product][group][0] = tr
+}
+
+type ProdStatus map[string]GroupStatus
+
+func makeProdStatus() ProdStatus {
+	return make(map[string]GroupStatus)
+}
+
+type GroupStatus map[string][]*TestResult
+
+func makeGroupStatus() GroupStatus {
+	return make(map[string][]*TestResult)
+}
+
+type persister struct {
+	conf     *ConfOptions
+	incoming chan *TestResult
+	results  chan *TestResult
+	status   status
+}
+
+func newPersister(cf *ConfOptions) *persister {
+	p := &persister{
+		conf:     cf,
+		status:   makeStatus(),
+		incoming: make(chan *TestResult),
+		results:  make(chan *TestResult),
+	}
+	go p.processIncoming(p.incoming)
+	go p.processResults(p.results)
+	return p
+}
+
+// Events coming from other agents
+func (p *persister) processIncoming(in <-chan *TestResult) {
+	for tr := range in {
+		p.status.appendTestResult(tr.Host, tr.Product, tr.Group, tr)
+		data, err := json.Marshal(p.status)
+		if err != nil {
+			log.Fatalf("cannot marshal tests status: %s", err)
+		}
+		if err := writeFileAtomic(p.conf.File, data, os.FileMode(0755)); err != nil {
+			log.Fatalf("cannot write results JSON file: %s", err)
+		}
+	}
+}
+
+// Events coming from our tests
+func (p *persister) processResults(in <-chan *TestResult) {
+	for r := range in {
+		errs, err := p.putRemotes(p.conf.Remotes, r)
+		if err != nil {
+			log.Fatalf("cannot put result to remote: %s", err)
+		}
+		// TODO: errs should have the url etc
+		for e := range errs {
+			log.Printf("put failed: %s", errs[e])
+		}
+		p.incoming <- r
+	}
+}
+
+func (p *persister) putRemotes(remotes []string, result *TestResult) ([]error, error) {
+	data, err := json.Marshal(result)
+	if err != nil {
+		return nil, fmt.Errorf("cannot marshal result: %s", err)
+	}
+	var errs []error
+	for i := range remotes {
+		r := bytes.NewBuffer(data) // TODO: optimize
+		err = p.putRemote(remotes[i], r)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	return errs, nil
+}
+
+func (p *persister) putRemote(url string, r io.Reader) error {
+	req, err := http.NewRequest("PUT", url, r)
+	if err != nil {
+		return fmt.Errorf("cannot PUT result: %s", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("cannot PUT result: %s", err)
+	}
+	defer resp.Body.Close()
+	// TODO: don't discard body if status != 200
+	io.Copy(ioutil.Discard, resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("PUT of result to %s: server returned status %d", url, resp.StatusCode)
+	}
+	return nil
+}
+
+func (p *persister) receiveTestResult(req *http.Request) error {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return fmt.Errorf("cannot read request body: %s", err)
+	}
+	tr := &TestResult{}
+	if err := json.Unmarshal(body, &tr); err != nil {
+		return fmt.Errorf("cannot unmarshal into result object: %s", err)
+	}
+	p.incoming <- tr
+	return err
+}
+
+type api struct {
+	persister *persister
+}
+
+func (a *api) handlePutResult(rw http.ResponseWriter, req *http.Request) {
+	if req.Method != "PUT" {
+		rw.Header().Set("Allow", "PUT")
+		http.Error(rw, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if err := a.persister.receiveTestResult(req); err != nil {
+		http.Error(rw, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	io.WriteString(rw, "Success\n") // TODO: check error
+}
+
 func main() {
 	flag.Parse()
 	arg := flag.Arg(0)
@@ -202,29 +385,20 @@ func main() {
 		log.Printf("cannot use configuration: %s", err)
 	}
 
-	fmt.Printf("%+v\n%+v\n", prods, opts)
+	apiPutResultPath := "/api/v0/results"
 
-	/*
-	for i := range cf {
-		res, err := checks[i].run()
-		if err != nil {
-			log.Printf("Error: can't run check %s: %s", checks[i].Name, err)
-			continue
+	opts.addRemotesPath(apiPutResultPath)
+	pr := newPersister(opts)
+
+	go func() {
+		for {
+			prods.runCheckers(hname, pr.results)
+			// TODO: must come from tests, this is more complicated
+			time.Sleep(1 * time.Second)
 		}
-		for c := range res {
-			fmt.Printf("%+v\n", res[c])
-		}
-	}
-	*/
+	}()
 
-	// -> read incoming results from JSON, put to incoming channel
-	// -> generate results by running tests, put to results channel
-	// -> receive from incoming channel, persist to JSON
-	// -> receive from results channel, duplicate and
-	//    -> put to incoming channel if result is local
-	//    -> put to all remote URLs that result carries
-	// -> persist a log with the latest errors, maybe?
-
-	//http.HandleFunc("/check/result/add", postCheckResult)
-	//log.Fatal(http.ListenAndServe(":8082", nil))
+	api := &api{pr}
+	http.HandleFunc(apiPutResultPath, api.handlePutResult)
+	log.Fatal(http.ListenAndServe(":8911", nil))
 }
